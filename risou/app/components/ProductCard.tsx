de343a727404ede4109b3d30a992fa0ef66ce15c
tabadines00 <thomas.abadines@gmail.com>
2023-10-24 23:49:10 -0700

    Set up DaisyUI

--- risou/app/components/ProductCard.tsx
+++ risou/app/components/ProductCard.tsx
@@ -6,7 +6,7 @@ import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
+    <div>
         <AddToCart />
     </div>
   )
