4871c87bcc06908bbfdd1ad04116f2086d5a05c1
tabadines00 <thomas.abadines@gmail.com>
2023-12-02 11:54:23 -0800

    implemented drag and drop

--- risou/app/components/ProductCard.tsx
+++ risou/app/components/ProductCard.tsx
@@ -1,13 +1,9 @@
 
 import React from 'react'
-import AddToCart from './AddToCart'
-
-import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
     <div>
-        <AddToCart />
     </div>
   )
 }
--- risou/app/users/page.tsx
+++ risou/app/users/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
+
 
 interface User {
 	id: number;
@@ -6,21 +9,82 @@ interface User {
 
 }
 
-const UsersPage = async () => {
+const sections = [
+	{
+		id: 'education',
+		sectionType: 'header',
+		title: 'Education',
+		content: 'San Francisco State University'
+	},
+	{
+		id: 'experience',
+		sectionType: 'header',
+		title: 'Relevant Experience',
+		content: ''
+	},
+	{
+		id: 'projects',
+		sectionType: 'header',
+		title: 'Relevant Projects',
+		content: ''
+	}
+]
 
-	const res = await fetch(
-		'https://jsonplaceholder.typicode.com/users',
-		{ next: { revalidate: 10 }})
-	const users: User[] = await res.json();
- 
+const UsersPage = () => {
+
+	const [sectionOrder, setSectionOrder] = useState(sections)
+	const [domReady, setDomReady] = useState(false)
+	
+	useEffect(() => {
+		setDomReady(true)
+	}, [])
+
+	function handleOnDragEnd(result) {
+		if(!result.destination) {
+			return
+		}
+		console.log(result)
+		const items = Array.from(sectionOrder)
+		const [reorderedItem] = items.splice(result.source.index, 1)
+		items.splice(result.destination.index, 0, reorderedItem)
+		setSectionOrder(items)
+	}
+
+	//const res = await fetch(
+	//	'https://jsonplaceholder.typicode.com/users',
+	//	{ next: { revalidate: 10 }})
+	//const users: User[] = await res.json();
+
+	 
 	return (
-		<>
-			<h1>Users</h1>
-			<p>{ new Date().toLocaleTimeString() }</p>
-			<ul>
-				{users.map(user=> <li key={user.id}>{user.name}</li>)}
-			</ul>
-		</>
+		<div>
+		{domReady ? <DragDropContext onDragEnd={handleOnDragEnd}>
+			<Droppable droppableId='sections'>
+				{(provided) => (
+				<ul {...provided.droppableProps} ref={provided.innerRef}>
+					{sectionOrder.map((section, index) => {
+						return (
+							<Draggable key={section.id} draggableId={section.id} index={index}>
+								{(provided) => (
+									<li>
+										<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='p-4 bg-slate-500'>
+											<h1>{section.title}</h1>
+										</div>
+									</li>
+								)}
+							</Draggable>
+						)
+					})}
+					{provided.placeholder}
+				</ul>
+				)}
+			</Droppable>
+		</DragDropContext> : null}
+		</div>
 	)
 }
 
